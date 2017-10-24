d6c10be63b3da22db0bab87e6bc1d8e73217c465
karate-island <asprocter@gmail.com>
2017-10-24 19:39:53 +0200

    new js
    created user var

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var user = {
+	firstName: "alexandria",
+	lastName: "procter",
+	emailAddress: "asporcter@gmail.com",
+	age: 24,
+	property: [{
+		location: "london",
+		type: "townhouse"
+	}, {
+		location: "kenya",
+		type: "farm"
+	}, {
+		location: "cape town",
+		type: "apartment"
+	}],
+	gender: "female"
+};
