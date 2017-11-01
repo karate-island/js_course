b48b354d5d1431c158cdafcf3fa7bd45a62cdd1e
karate-island <asprocter@gmail.com>
2017-11-01 20:20:55 +0200

    mixing functions and loops

--- js/main.js
+++ js/main.js
@@ -21,6 +21,23 @@ var user = {
 	gender: "male",
 	maritalStatus: "divorced",
 	exwives: ["sally", "georgia", "ophelia", "bill"],
+	children: [{
+		name: 'Mark',
+		age: 21,
+		amount: 3213
+	}, {
+		name: 'Mary',
+		age: 18,
+		amount: 3000
+	}, {
+		name: 'John',
+		age: 12,
+		amount: 1980
+	}, {
+		name: 'James',
+		age: 2,
+		amount: 1200
+	}]
 };
 
 console.log(user.firstName + " " + user.lastName);
@@ -195,6 +212,59 @@ for(i=0;i<user.property.length;i++) {
 	}
 }
 
+
+//-------------------- mixing functions and loops ---------------------//
+
+function percentageNumber(number, percent) {
+	return (percent / 100) * number
+}
+
+function maintenanceDeduction(user) {
+	// Loop through the user's children
+	   // If child is younger than 18,
+	   // add the amount to the total maintenance amount
+	// Return the total maintenance amount
+
+	if(!user) {
+		return;
+	}
+
+	var maintenanceAmount = 0;
+
+	if(user.children && typeof(user.children) == 'object') {
+		console.log('--- Inclusion on if');
+		for(i=0;i<user.children.length;i++) {
+			console.log('--- Within loop. Index: ' + i);
+			var child = user.children[i];
+
+			if(child.age && child.amount) {
+				if(child.age < 18) {
+					maintenanceAmount += child.amount
+				}
+			}
+
+			console.log(child);
+		}
+	} else {
+		console.log('--- Exclusion on if');
+	}
+
+	return maintenanceAmount;
+}
+
+function calculateDeductions(user) {
+	return maintenanceDeduction(user) + percentageNumber(user.salary, 14)
+}
+
+var salary = user.salary,
+		deductions = calculateDeductions(user),
+		salaryAfterDeductions = salary - deductions;
+
+alert('Get ready');
+
+var salaryDiv = document.getElementById('userSalary');
+
+salaryDiv.innerHTML = salary;
 
 
 
